b009d41839d6feb44e7a56f7b66c6f7ce58ea785
vicenteramirezg <vicente.ramirezg@gmail.com>
2025-04-02 10:56:34 -0400

    Hotfix.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -17,6 +17,9 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks: undefined,
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
       },
     },
     manifest: true,
